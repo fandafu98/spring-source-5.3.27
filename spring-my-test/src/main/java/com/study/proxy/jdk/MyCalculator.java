dabaecb73580d4d1229400500f99679e4fd63764
fandafu98 <792297455@qq.com>
2023-07-29 21:44:14 +0800

    动态代理测试

---
+++ spring-my-test/src/main/java/com/study/proxy/jdk/MyCalculator.java
@@ -0,0 +1,41 @@
+/**
+ * zhuyanyoushu.com
+ * Copyright (C) 2021-2023 All Rights Reserved.
+ *
+ * @author: 郑延康
+ * @date: 2023-07-24 19:57
+ */
+package com.study.proxy.jdk;
+
+/**
+ *
+ *
+ * @author zhengyankang
+ * @version MyCalculator.java, v 0.1 2023-07-24 19:57
+ */
+public class MyCalculator implements Calculator{
+
+	@Override
+	public int add(int i, int j) {
+		int result = i+j;
+		return result;
+	}
+
+	@Override
+	public int sub(int i, int j) {
+		int result = i-j;
+		return result;
+	}
+
+	@Override
+	public int mult(int i, int j) {
+		int result = i*j;
+		return result;
+	}
+
+	@Override
+	public int div(int i, int j) {
+		int result = i/j;
+		return result;
+	}
+}
