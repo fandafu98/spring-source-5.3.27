4fd112b4fe57699f4fadc30ab531a42fbfe53349
fandafu98 <792297455@qq.com>
2023-08-05 18:08:44 +0800

    Spring 事务

---
+++ spring-my-test/src/main/java/com/study/tx/dao/BookDao.java
@@ -0,0 +1,44 @@
+/**
+ * zhuyanyoushu.com
+ * Copyright (C) 2021-2023 All Rights Reserved.
+ *
+ * @author: 郑延康
+ * @date: 2023-08-02 20:42
+ */
+package com.study.tx.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ *
+ * @author zhengyankang
+ * @version BookDao.java, v 0.1 2023-08-02 20:42
+ */
+public class BookDao {
+
+	JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+
+	public void updateStock(int id){
+
+	}
+
+
+	public int getPrice(int id){
+		return 1;
+	}
+
+	public void updateBalance(String username,int price){
+
+	}
+
+}
